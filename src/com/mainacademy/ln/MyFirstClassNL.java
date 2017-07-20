47d86993a7080baf00a629b480aff3b50e22440a
lnplav <leonenko020@gmail.com>
2017-07-20 21:50:06 +0300

    update

--- src/com/mainacademy/ln/MyFirstClassNL.java
+++ src/com/mainacademy/ln/MyFirstClassNL.java
@@ -13,7 +13,25 @@ public class MyFirstClassNL {
         String line = sc.nextLine();
         System.out.println(line);
 
-    }
+
+        int a = 1;
+        int b = 4;
+        int sum = a + b;
+        System.out.println(sum);
+        boolean boolExpr = a < b;
+        // Two options
+        if (boolExpr){
+            System.out.println("a < b - true");
+        } else {
+            System.out.println("a < b - false");
+        }
+
+
+
+
+
+
 
     }
+}
 

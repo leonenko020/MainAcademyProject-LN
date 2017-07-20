d615c15575a1e0b5b2f9a55ca1346613a9191895
lnplav <leonenko020@gmail.com>
2017-07-20 20:35:30 +0300

    update 200717

---
+++ src/com/mainacademy/ln/MyFirstClassNL.java
@@ -0,0 +1,19 @@
+package com.mainacademy.ln;
+
+import java.util.Scanner;
+
+/**
+ * Created by 110-15 on 20.07.2017.
+ */
+public class MyFirstClassNL {
+    public static void main(String[] args) {
+
+        System.out.println("Hello Brain Academy!");
+        Scanner sc = new Scanner(System.in);
+        String line = sc.nextLine();
+        System.out.println(line);
+
+    }
+
+    }
+

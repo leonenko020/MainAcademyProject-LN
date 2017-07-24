e9c6142752b7a4c64e666a90bd75d72fcce7405f
lnplav <leonenko020@gmail.com>
2017-07-24 23:10:32 +0300

    summa = 12
    Закомментировать часть кода, чтобы на экран вывелось сообщение «summa = 12»

--- src/com/mainacademy/ln/MyFirstClassNL.java
+++ src/com/mainacademy/ln/MyFirstClassNL.java
@@ -1,6 +1,5 @@
 package com.mainacademy.ln;
 
-import java.util.Scanner;
 
 /**
  * Created by 110-15 on 20.07.2017.
@@ -10,12 +9,15 @@ public class MyFirstClassNL {
 
     public static void main(String[] args) {
 
-        int a = 1;
-        int b = 3;
-        int c = 9;
-        int d = 27;
-        int result = - a + b - c + d;
-        System.out.println(result);}}
+        int a = 3;
+        // int a = 10;
+        int b = 6;
+        b = 12;
+        int summa = 1 + a + b;
+        summa = 2 + a + b;
+        summa = 3 + a + b;
+        //int summa = 4 + a + b;
+        System.out.println("summa = " + summa);}}
 
 
 

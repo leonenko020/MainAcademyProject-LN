dfdf814be9ebc6e150c1f912032f434df477e756
lnplav <leonenko020@gmail.com>
2017-07-24 22:44:55 +0300

    Display name
    Объявить переменную name типа String

--- src/com/mainacademy/ln/MyFirstClassNL.java
+++ src/com/mainacademy/ln/MyFirstClassNL.java
@@ -5,26 +5,17 @@ import java.util.Scanner;
 /**
  * Created by 110-15 on 20.07.2017.
  */
+
 public class MyFirstClassNL {
+
     public static void main(String[] args) {
-
-        System.out.println("Hello Brain Academy!");
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        System.out.println(line);
+        String name = "Natalia";
+        System.out.println(name);}}
 
 
-        int a = 1;
-        int b = 4;
-        int sum = a + b;
-        System.out.println(sum);
-        boolean boolExpr = a < b;
-        // Two options
-        if (boolExpr){
-            System.out.println("a < b - true");
-        } else {
-            System.out.println("a < b - false");
-        }
+
+
+
 
 
 

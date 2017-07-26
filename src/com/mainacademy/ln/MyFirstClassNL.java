456dba104569526edd7f6bbddf5a3ff0df8fc6e7
lnplav <leonenko020@gmail.com>
2017-07-26 21:59:50 +0300

    if else
    if else

--- src/com/mainacademy/ln/MyFirstClassNL.java
+++ src/com/mainacademy/ln/MyFirstClassNL.java
@@ -8,14 +8,37 @@ package com.mainacademy.ln;
 public class MyFirstClassNL {
 
     public static void main(String[] args) {
-        //String s = "15";
-        int a = 5;
-//int z = 18;
-        int d = 18;
-        int c = a + d;
-        String b = " better then ";
-        System.out.println(a + b + c);}}
+        //Площадь квадрата
+        float a = 2;
+        float square = a*a;
+        System.out.println(square);
 
+        //Площадь прямоугольника
+        int d = 2;
+        int b = 3;
+        int c = d*b;
+        System.out.println(c);//
+
+        //int x=10;
+        //int y=20;
+        //String S1 = "y-x не больше 10";
+        //if ((y-x)>11) {
+        //
+        //System.out.println(S1);}
+        //
+        //else if ((y-x)==10){String S2 = "y-x равно 10";
+        //System.out.println(S2);
+        //}
+        //else {
+        //String S3 = "y-x не равно ";
+        //System.out.println(S3);
+        int i =20;
+        while ( i> 10)
+            System.out.println("lteration:" + i);
+            i--;
+        for (int j=8; j<10; j+=3) {
+            System.out.println("lteration:" + j);
+        }}}
 
 
 
